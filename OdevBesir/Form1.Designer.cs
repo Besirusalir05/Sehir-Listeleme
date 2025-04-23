271900ddb8cbdf2ec220a8f5ac3e9a97805f7950
Besirusalir05 <usalirbesir816@gmail.com>
2025-04-24 00:22:29 +0300

    Add files via upload

---
+++ OdevBesir/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace OdevBesir
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.lblİller2 = new System.Windows.Forms.ListBox();
+            this.lblİller = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtİl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(800, 73);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Şehir Uygulaması";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLeft.Location = new System.Drawing.Point(372, 310);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 26);
+            this.btnLeft.TabIndex = 14;
+            this.btnLeft.Text = "<<";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRight.Location = new System.Drawing.Point(372, 209);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 28);
+            this.btnRight.TabIndex = 13;
+            this.btnRight.Text = ">>";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // lblİller2
+            // 
+            this.lblİller2.FormattingEnabled = true;
+            this.lblİller2.ItemHeight = 16;
+            this.lblİller2.Location = new System.Drawing.Point(457, 179);
+            this.lblİller2.Name = "lblİller2";
+            this.lblİller2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lblİller2.Size = new System.Drawing.Size(198, 196);
+            this.lblİller2.TabIndex = 12;
+            // 
+            // lblİller
+            // 
+            this.lblİller.FormattingEnabled = true;
+            this.lblİller.ItemHeight = 16;
+            this.lblİller.Location = new System.Drawing.Point(164, 179);
+            this.lblİller.Name = "lblİller";
+            this.lblİller.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lblİller.Size = new System.Drawing.Size(198, 196);
+            this.lblİller.TabIndex = 11;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(457, 124);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(88, 29);
+            this.btnEkle.TabIndex = 10;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtİl
+            // 
+            this.txtİl.Location = new System.Drawing.Point(302, 127);
+            this.txtİl.Name = "txtİl";
+            this.txtİl.Size = new System.Drawing.Size(149, 22);
+            this.txtİl.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(263, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "İL:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.lblİller2);
+            this.Controls.Add(this.lblİller);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtİl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.ListBox lblİller2;
+        private System.Windows.Forms.ListBox lblİller;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtİl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
